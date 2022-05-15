dd0c12a71034cf61c76b9e6fb042342876bc4dd8
Purnima Mohapatra <mohapatrapurnima95@gmail.com>
2022-05-15 20:39:42 +0530

    Without DB

--- src/main/java/com/jaza/todoapp/restcontroller/TaskRestController.java
+++ src/main/java/com/jaza/todoapp/restcontroller/TaskRestController.java
@@ -11,27 +11,55 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
+@RequestMapping("/tasks")
 public class TaskRestController {
-    private TaskService toDoService;
-
     @Autowired
-    public TaskRestController(TaskService toDoService) {
-        this.toDoService = toDoService;
-    }
+     TaskService taskService;
 
+ //   @Autowired
+//    public TaskRestController(TaskService toDoService) {
+//        this.toDoService = toDoService;
+//    }
+
+//    @ResponseStatus(HttpStatus.CREATED)
+//    @PostMapping
+//    public ResponseEntity<Void> createTask(@RequestBody Task task){
+//        Task taskCreated = taskService.save(task);
+//        URI uri = URI.create("/tasks/"+ taskCreated.getId());
+//        return ResponseEntity.created(uri).build();
+//    }
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/ToDos")
-    public ResponseEntity<Void> createTask(@RequestBody Task task){
-        Task taskCreated = toDoService.save(task);
-         URI uri = URI.create("/ToDos/"+ taskCreated.getId());
-        return ResponseEntity.created(uri).build();
+    @PostMapping
+    public Task createTask(@RequestBody Task task){
+        return taskService.save(task);
     }
 
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping("/ToDos/{id}")
+    @GetMapping("/getAllTasks")
+    private List<Task> getAllTasks() {
+        return taskService.findAll();
+    }
+
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/{id}")
     public ResponseEntity<Task> getTask(@PathVariable ("id") Long id){
-        Task task = toDoService.findOne(id);
+        Task task = taskService.findOne(id);
         return new ResponseEntity<>(task ,HttpStatus.OK);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable("id") Long id, @RequestBody Task task){
+        task= taskService.update(id, task);
+        return ResponseEntity.ok(task);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable("id") Long id){
+        taskService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
